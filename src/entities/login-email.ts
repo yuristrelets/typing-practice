44b5a2f6efb5ac540d34df446238d1c462ab1729
yuristrelets <yuristrelets@gmail.com>
2021-03-17 22:14:47 +0200

    simplify constructors

--- src/entities/login-email.ts
+++ src/entities/login-email.ts
@@ -1,18 +1,15 @@
 import { isString } from "../utils/is-string";
 
 export class LoginEmail {
-  private readonly _type = Symbol("login email");
-  public value: string;
-
   static from(value: any): LoginEmail {
     if (isString(value)) {
-      return new LoginEmail(value);
+      return new LoginEmail(value.toLowerCase());
     }
 
     throw new Error("Email must be a string!");
   }
 
-  private constructor(email: string) {
-    this.value = email.toLowerCase();
-  }
+  private readonly _type = Symbol("login email");
+
+  private constructor(public readonly value: string) {}
 }
--- src/entities/login-password.ts
+++ src/entities/login-password.ts
@@ -1,9 +1,6 @@
 import { isString } from "../utils/is-string";
 
 export class LoginPassword {
-  private readonly _type = Symbol("login password");
-  public value: string;
-
   static from(value: any): LoginPassword {
     if (isString(value)) {
       return new LoginPassword(value);
@@ -12,7 +9,7 @@ export class LoginPassword {
     throw new Error("Password must be a string!");
   }
 
-  private constructor(password: string) {
-    this.value = password;
-  }
+  private readonly _type = Symbol("login password");
+
+  constructor(public readonly value: string) {}
 }
